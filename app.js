a3922d87a7fd66ae09aff6547d4ffb97c6d7c55f
mozoby <bt.mozoby@gmail.com>
2014-04-12 22:48:43 -0700

    iniit done

---
+++ app.js
@@ -0,0 +1,47 @@
+var exec = require('child-process').exec;
+var http = require('http');
+var fs = require('fs');
+
+//set up http server for receiving git post-push events for auto-deployment to the server
+http.createServer(function(req,res) {
+	if(req.url=='/gitpull'){
+		child = exec('git pull', function(error,stdout,stderr){
+			fs.writeFile("./gitresults.txt", stdout, function(err){
+				//do nothing
+			});
+		});
+	}
+});
+
+//set up websocket to listen for new hosts
+var io = require('socket.io').listen(8080);
+
+var clientCount = 0;
+
+io.sockets.on('connection', function(socket) {
+	clientCount += 1;
+	socket.on('initialize', function(userId) {
+		socket.set('userId', userId, function(){
+			socket.emit('update', {clusterCount: clientCount});
+		});
+		socket.set('clusterID',null);
+		socket.set('answer',null);
+	});
+
+	socket.on('disconnect',function(data){
+		clientCount -= 1;
+	});
+
+	socket.on('answer',function(data){
+		socket.set('answer', data.answer);
+	});
+});
+
+setInterval(function() {
+	//group users with same answers who already in the same cluster
+	// or with users who have no cluster
+	//set null all answers
+
+	//set 
+
+}, delay,15000);
