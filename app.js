37c797ab67a80933a19ed51431d54b6b0a363346
mozoby <bt.mozoby@gmail.com>
2014-04-13 01:59:53 -0700

    should be done but probably isnt

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ var exec = require('child_process').exec;
 var http = require('http');
 var fs = require('fs');
 
+
 function uuid(){
-	var s = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-	    return v.toString(16);
-	});
-	return s;
+    var s = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+    return s;
 }
 
 //set up http server for receiving git post-push events for auto-deployment to the server
 http.createServer(function(req,res) {
-	if(req.url=='/gitpull'){
-		child = exec('git pull', function(error,stdout,stderr){
-			fs.writeFile("./gitresults.txt", stdout, function(err){
-				//do nothing
-			});
-		});
-	}
+    if(req.url=='/gitpull'){
+        child = exec('git pull', function(error,stdout,stderr){
+            fs.writeFile("./gitresults.txt", stdout, function(err){
+                //do nothing
+            });
+        });
+    }
 });
 
 //set up websocket to listen for new hosts
@@ -28,121 +29,142 @@ var clientCount = 0;
 var isStarted = false;
 
 io.sockets.on('connection', function(socket) {
-	clientCount += 1;
-	if(clientCount > 4)
-		isStarted = true;
+    clientCount += 1;
+    if(clientCount > 4)
+        isStarted = true;
 
-	console.log('connection established: ' + clientCount );
+    console.log('connection established: ' + clientCount );
 
-	socket.on('initialize', function(userId) {
-		
-		socket.set('userId', userId, function(){
-			console.log(userId);
-		});
-		socket.emit('update', {clusterCount: clientCount});
-		//console.log(socket.get('userId'));
-	});
+    socket.on('initialize', function(userId) {
+        
+        socket.set('userId', userId, function(){
+            console.log(userId);
+        });
+        socket.emit('update', {clusterCount: clientCount});
+        //console.log(socket.get('userId'));
+    });
 
-	socket.on('disconnect',function(data){
-		clientCount -= 1;
-	});
+    socket.on('disconnect',function(data){
+        clientCount -= 1;
+    });
 
-	socket.on('answer',function(data){
-		socket.set('answer', data.answer);
-	});
+    socket.on('answer',function(data){
+        socket.set('answer', data.answer);
+    });
 });
+
+var natural = require('natural');
+var string_similarity_threshold = 0.72;
 
 var questionIndex = 0;
 var questions = [
-	{question: "Question 1?", answers:['1','2','3','4']},
-	{question: "Question 2?", answers:['1','2','3','4']},
-	{question: "Question 3?", answers:['1','2','3','4']},
-	{question: "Question 4?", answers:['1','2','3','4']},
-	{question: "Question 5?", answers:['1','2','3','4']},
-	{question: "Question 6?", answers:['1','2','3','4']},
-	{question: "Question 7?", answers:['1','2','3','4']},
-	{question: "Question 8?", answers:['1','2','3','4']},
-	{question: "Question 9?", answers:['1','2','3','4']},
-	{question: "Question 10?", answers:['1','2','3','4']},
-	{question: "Question 11?", answers:['1','2','3','4']},
-	{question: "Question 12?", answers:['1','2','3','4']},
-	{question: "Question 13?", answers:['1','2','3','4']},
-	{question: "Question 14?", answers:['1','2','3','4']}
+    {question: "Question 1?", answers:['1','2','3','4']},
+    {question: "Question 2?", answers:['1','2','3','4']},
+    {question: "Question 3?", answers:['1','2','3','4']},
+    {question: "Question 4?", answers:['1','2','3','4']},
+    {question: "Question 5?", answers:['1','2','3','4']},
+    {question: "Question 6?", answers:['1','2','3','4']},
+    {question: "Question 7?", answers:['1','2','3','4']},
+    {question: "Question 8?", answers:['1','2','3','4']},
+    {question: "Question 9?", answers:['1','2','3','4']},
+    {question: "Question 10?", answers:['1','2','3','4']},
+    {question: "Question 11?", answers:['1','2','3','4']},
+    {question: "Question 12?", answers:['1','2','3','4']},
+    {question: "Question 13?", answers:['1','2','3','4']},
+    {question: "Question 14?", answers:['1','2','3','4']}
 ];
 
 var startInterval = setInterval(function() {
-	if(isStarted){
-		setInterval(function(){
-			//group users with same answers
-			if(io.sockets.manager.rooms.length > 0){
-				console.log(io.sockets.manager.rooms);
-				Object.keys(io.sockets.manager.rooms).forEach(function(roomName){
-					
-					newRooms = [{name:uuid(), count: 0},
-						{name:uuid(), count: 0},
-						{name:uuid(), count: 0},
-						{name:uuid(), count: 0}];
+    if(isStarted){
+        setInterval(function(){
+            //group users with same answers
+            if(io.sockets.manager.rooms.length > 0){
+                console.log(io.sockets.manager.rooms);
+                Object.keys(io.sockets.manager.rooms).forEach(function(roomName){
+                    
+                    newRooms = []; // {answer:'', name:''}
+                    roomClientCount = io.sockets.clients(roomName).length;
+                    clientsProcessed = 0;
+                    io.sockets.clients(roomName).forEach(function (socket) {
+                        socket.leave(roomName);
+                        socket.get('answer', function (err, answer) {
+                            joined = false;
+                            newRooms.forEach(roomObj){
+                                if(natural.JaroWinklerDistance(answer,roomObj.answer) >= string_similarity_threshold){
+                                    joined = true;
+                                    socket.join(roomObj.name);
+                                    break;
+                                }
+                            }
+                            if(!joined){
+                                newName = uuid();
+                                newRooms.append({answer:answer, name: newName});
+                                socket.join(newName);
+                            }
+                        });
+                    });
+                });
+            }else{
+                //users which have no room assigned yet (beginning only)
+                newRooms = []; // {answer:'', name:''}
+                io.sockets.clients().forEach(function (socket) {
+                    socket.get('answer', function (err, answer) {
+                        joined = false;
+                        newRooms.forEach(roomObj){
+                            if(natural.JaroWinklerDistance(answer,roomObj.answer) >= string_similarity_threshold){
+                                joined = true;
+                                socket.join(roomObj.name);
+                                break;
+                            }
+                        }
+                        if(!joined){
+                            newName = uuid();
+                            newRooms.append({answer:answer, name: newName});
+                            socket.join(newName);
+                        }
+                    });
+                });
 
-					io.sockets.clients(roomName).forEach(function (socket) {
-						socket.leave(roomName);
-						socket.get('answer', function (err, answer) {
-					      	socket.join(newRooms[answer].name);
-					      	newRooms[answer].count += 1;
-					    });
-					});
-				});
-			}else{
-				//users which have no room assigned yet (beginning only)
-				newRooms = [{name:uuid(), count: 0},
-					{name:uuid(), count: 0},
-					{name:uuid(), count: 0},
-					{name:uuid(), count: 0}];
+                
+            }
+            //set timeout in order to accomodate th non-blocking io of socket.get
+            setTimeout(function(){
+                //update all users with cluster count
+                console.log(io.sockets.manager.rooms);
+                Object.keys(io.sockets.manager.rooms).forEach(function(roomName){
+                    io.sockets.clients(roomName).forEach(function(socket){
+                        socket.emit('update', {clusterCount:io.sockets.clients(roomName).length});
+                    });
 
-				io.sockets.clients().forEach(function (socket) {
-					socket.get('answer', function (err, answer) {
-				      	socket.join(newRooms[answer].name);
-				      	newRooms[answer].count += 1;
-				    });
-				});
+                    //if theres less than or equal to 2 people in the room exchange userIds
+                    if(io.sockets.clients(roomName).length <= 2){
+                        roomId = uuid();
+                        io.sockets.clients(roomName).forEach(function(socket){
+                            io.sockets.clients(roomName).forEach(function(socket2){
+                                if(socket2.id != socket.id){
+                                    socket2.get('userId',function(err,userId){
+                                        socket.emit('finalize', {roomId:roomId});
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
 
-				
-			}
-			//set timeout in order to accomodate th non-blocking io of socket.get
-			setTimeout(function(){
-				//update all users with cluster count
-				console.log(io.sockets.manager.rooms);
-				Object.keys(io.sockets.manager.rooms).forEach(function(roomName){
-					io.sockets.clients(roomName).forEach(function(socket){
-						socket.emit('update', {clusterCount:io.sockets.clients(roomName).length});
-					});
+                //set all answers to null
+                io.sockets.clients().forEach(function (socket) {
+                    socket.set('answer',null);
+                });
 
-					//if theres less than or equal to 2 people in the room exchange userIds
-					if(io.sockets.clients(roomName).length <= 2){
-						userIds = [];
-						io.sockets.clients(roomName).forEach(function(socket){
-							io.sockets.clients(roomName).forEach(function(socket2){
-								if(socket2.id != socket.id){
-									socket2.get('userId',function(err,userId){
-										socket.emit('finalize', {friendId:userId});
-									});
-								}
-							});
-						});
-					}
-				});
-			},1000);
-			
+                //broadcast next question
+                io.sockets.emit('question', questions[questionIndex]);
 
-			//set all answers to null
-			io.sockets.clients().forEach(function (socket) {
-				socket.set('answer',null);
-			});
+                questionIndex ++;
+            },3000);
+            
 
-			//broadcast next question
-			io.sockets.emit('question', questions[questionIndex]);
-
-			questionIndex ++;
-		},15000);
-		clearInterval(startInterval);
-	}
+            
+        },12000);
+        clearInterval(startInterval);
+    }
 },30000);
