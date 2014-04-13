9c9fa47e059bc2bc7da092fc96384ef8f2a9bf73
mozoby <bt.mozoby@gmail.com>
2014-04-13 10:55:33 -0700

    questions

--- app.js
+++ app.js
@@ -29,16 +29,18 @@ http.createServer(function(req,res) {
             res.end();
         });
     }else{
-        if(fs.existsSync('app' + req.url)){
-            fs.readFile('app' + req.url, function(err, page) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(page);
+        fs.exists('app' + req.url,function(exists){
+            if(exists){
+                fs.readFile('app' + req.url, function(err, page) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(page);
+                    res.end();
+                });
+            }else{
+                res.writeHead(400);
                 res.end();
-            });
-        }else{
-            res.writeHead(400);
-            res.end();
-        }
+            }
+        });
     }
 }).listen(80,'0.0.0.0');
 
